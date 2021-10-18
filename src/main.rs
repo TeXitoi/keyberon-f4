4c35bbf9a5f6908ff1ce8537837c1c72059e0753
Guillaume Pinot <guillaume.pinot@citodi.fr>
2021-10-18 11:30:30 +0200

    Dedicated layout.rs file + bootloader instead of reset

--- src/main.rs
+++ src/main.rs
@@ -4,106 +4,23 @@
 // set the panic handler
 use panic_halt as _;
 
-use keyberon::action::{k, l, m, Action, Action::*, HoldTapConfig};
 use keyberon::debounce::Debouncer;
 use keyberon::key_code::KbHidReport;
-use keyberon::key_code::KeyCode::{self, *};
+use keyberon::key_code::KeyCode;
 use keyberon::layout::Layout;
 use keyberon::matrix::{Matrix, PressedKeys};
 use rtic::app;
 use stm32f4xx_hal::gpio::{self, EPin, Input, Output, PullUp, PushPull};
 use stm32f4xx_hal::otg_fs::{UsbBusType, USB};
 use stm32f4xx_hal::prelude::*;
-use stm32f4xx_hal::{stm32, timer};
+use stm32f4xx_hal::{pac, timer};
 use usb_device::bus::UsbBusAllocator;
 use usb_device::class::UsbClass as _;
 
+mod layout;
+
 type UsbClass = keyberon::Class<'static, UsbBusType, Leds>;
 type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
-
-const CUT: Action<()> = m(&[LShift, Delete]);
-const COPY: Action<()> = m(&[LCtrl, Insert]);
-const PASTE: Action<()> = m(&[LShift, Insert]);
-const L2_ENTER: Action<()> = HoldTap {
-    timeout: 200,
-    config: HoldTapConfig::HoldOnOtherKeyPress,
-    tap_hold_interval: 0,
-    hold: &l(2),
-    tap: &k(Enter),
-};
-const L1_SP: Action<()> = HoldTap {
-    timeout: 200,
-    config: HoldTapConfig::Default,
-    tap_hold_interval: 0,
-    hold: &l(1),
-    tap: &k(Space),
-};
-const CSPACE: Action<()> = m(&[LCtrl, Space]);
-
-const SHIFT_ESC: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LShift),
-    tap: &k(Escape),
-};
-const CTRL_INS: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LCtrl),
-    tap: &k(Insert),
-};
-const ALT_NL: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LAlt),
-    tap: &k(NumLock),
-};
-
-macro_rules! s {
-    ($k:ident) => {
-        m(&[LShift, $k])
-    };
-}
-macro_rules! a {
-    ($k:ident) => {
-        m(&[RAlt, $k])
-    };
-}
-
-// The 13th column is the hardware button of the development board,
-// thus all the column is activated when the button is pushed. Because
-// of that, only one action is defined in the 13th column.
-#[rustfmt::skip]
-pub static LAYERS: keyberon::layout::Layers<()> = &[
-    &[
-        &[k(Grave),  k(Kb1),k(Kb2),k(Kb3),  k(Kb4),k(Kb5), k(Kb6),   k(Kb7),  k(Kb8), k(Kb9),  k(Kb0),   k(Minus),  k(Space)],
-        &[k(Tab),     k(Q), k(W),  k(E),    k(R), k(T),    k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A), k(S),  k(D),    k(F), k(G),    k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z), k(X),  k(C),    k(V), k(B),    k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),L2_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
-    ], &[
-        &[k(F1),         k(F2),   k(F3), k(F4),     k(F5),    k(F6),k(F7),      k(F8),    k(F9),    k(F10), k(F11),  k(F12)],
-        &[Trans,         k(Pause),Trans, k(PScreen),Trans,    Trans,Trans,      k(BSpace),k(Delete),Trans,  Trans,   Trans ],
-        &[Trans,         Trans,   ALT_NL,CTRL_INS,  SHIFT_ESC,Trans,k(CapsLock),k(Left),  k(Down),  k(Up),  k(Right),Trans ],
-        &[k(NonUsBslash),k(Undo), CUT,   COPY,      PASTE,    Trans,Trans,      k(Home),  k(PgDown),k(PgUp),k(End),  Trans ],
-        &[Trans,         Trans,   Trans, Trans,     Trans,    Trans,Trans,      Trans,    Trans,    Trans,  Trans,   Trans ],
-    ], &[
-        &[Trans,    Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
-        &[s!(Grave),s!(Kb1),s!(Kb2),s!(Kb3),s!(Kb4),s!(Kb5),s!(Kb6),s!(Kb7),s!(Kb8),s!(Kb9),s!(Kb0),s!(Minus)],
-        &[ k(Grave), k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5), k(Kb6), k(Kb7), k(Kb8), k(Kb9), k(Kb0), k(Minus)],
-        &[a!(Grave),a!(Kb1),a!(Kb2),a!(Kb3),a!(Kb4),a!(Kb5),a!(Kb6),a!(Kb7),a!(Kb8),a!(Kb9),a!(Kb0),a!(Minus)],
-        &[Trans,    Trans,  Trans,  Trans,  CSPACE, Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
-    ], &[
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[k(F1),k(F2),k(F3),k(F4),k(F5),k(F6),k(F7),k(F8),k(F9),k(F10),k(F11),k(F12)],
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[Action::Custom(()),Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-    ],
-];
 
 pub struct Leds {
     caps_lock: gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
@@ -118,15 +35,15 @@ impl keyberon::keyboard::Leds for Leds {
     }
 }
 
-#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[app(device = stm32f4xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         usb_dev: UsbDevice,
         usb_class: UsbClass,
-        matrix: Matrix<EPin<Input<PullUp>>, EPin<Output<PushPull>>, 13, 5>,
-        debouncer: Debouncer<PressedKeys<13, 5>>,
+        matrix: Matrix<EPin<Input<PullUp>>, EPin<Output<PushPull>>, 13, 4>,
+        debouncer: Debouncer<PressedKeys<13, 4>>,
         layout: Layout<()>,
-        timer: timer::CountDownTimer<stm32::TIM3>,
+        timer: timer::CountDownTimer<pac::TIM3>,
     }
 
     #[init]
@@ -183,7 +100,6 @@ const APP: () = {
                 gpioa.pa0.into_pull_up_input().erase(),
             ],
             [
-                gpioa.pa4.into_push_pull_output().erase(),
                 gpioa.pa3.into_push_pull_output().erase(),
                 gpioa.pa2.into_push_pull_output().erase(),
                 gpioa.pa1.into_push_pull_output().erase(),
@@ -197,7 +113,7 @@ const APP: () = {
             timer,
             debouncer: Debouncer::new(PressedKeys::default(), PressedKeys::default(), 5),
             matrix: matrix.unwrap(),
-            layout: Layout::new(LAYERS),
+            layout: Layout::new(crate::layout::LAYERS),
         }
     }
 
@@ -223,7 +139,9 @@ const APP: () = {
             c.resources.layout.event(event);
         }
         match c.resources.layout.tick() {
-            keyberon::layout::CustomEvent::Release(()) => cortex_m::peripheral::SCB::sys_reset(),
+            keyberon::layout::CustomEvent::Release(()) => unsafe {
+                cortex_m::asm::bootload(0x1FFF0000 as _)
+            },
             _ => (),
         }
         send_report(c.resources.layout.keycodes(), &mut c.resources.usb_class);
